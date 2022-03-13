cf20ffab43e5299fd9fcf620d38c07031d89bb04
lonar <zceimlo@ucl.ac.uk>
2022-03-13 17:43:57 +0000

    Adding Blueprints for authentication and main page

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -8,15 +8,16 @@ login_manager = LoginManager()
 csrf = CSRFProtect()
 
 
-def create_app():
+def create_app(config_class_name):
     """
     Initialise the Flask application.
     :rtype: Returns a configured Flask object
     """
     app = Flask(__name__)
+    app.config.from_object(config_class_name)
 
     db.init_app(app)
-    login_manager.init_app(app)
+    # login_manager.init_app(app)
     csrf.init_app(app)
     csrf._exempt_views.add('dash.dash.dispatch')
 
@@ -24,5 +25,14 @@ def create_app():
         # Import Dash Application
         from dash_app.dashb_app import init_dashboard
         app = init_dashboard(app)
+
+        from my_app.models import User
+        db.create_all()
+
+    from my_app.main.routes import main_bp
+    app.register_blueprint(main_bp)
+
+    from my_app.auth.routes import auth_bp
+    app.register_blueprint(auth_bp)
 
     return app
